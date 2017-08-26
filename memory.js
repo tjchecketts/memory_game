ea0f212d59db28e97af02653c323472022104714
TJ Checketts <tylerchecketts@gmail.com>
2017-08-26 14:35:47 -0600

    first commit

---
+++ memory.js
@@ -0,0 +1,15 @@
+// $('box').on('click', (function() {
+//   var newImage = "yellow_triangle.jpg"
+
+//   '#a1' = newImage
+// }))
+
+
+// $(document).click(function () {
+//   var newImg = $(this).data('#a1');
+//   var imgURL = "yellow_triangle.jpg"
+//   $("#a1").html("<img src='" + imgUrl + "' alt='yellow triangle' />");
+
+// });
+
+
